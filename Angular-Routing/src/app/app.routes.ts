24c2a80a03f608862e452be5603b976b885095ac
GabrielePapagni <159022047+GabrielePapagni@users.noreply.github.com>
2025-09-29 08:57:39 +0000

    Aggiunta 2 bottoni

---
+++ Angular-Routing/src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes, RouterModule } from '@angular/router';
+import { AnimalsComponent } from './animals/animals.component';
+import { FruitsComponent } from './fruits/fruits.component';
+import { NgModule } from '@angular/core';
+
+export const routes: Routes = [
+  { path: 'animals', component: AnimalsComponent },
+  { path: 'fruits', component: FruitsComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
