cd82ccab4901c40dc880009e41cc7e55c10609f5
GabrielePapagni <159022047+GabrielePapagni@users.noreply.github.com>
2025-09-29 10:46:43 +0000

    Parte2 - in corso

--- Angular-Routing/src/app/app.routes.ts
+++ Angular-Routing/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes, RouterModule } from '@angular/router';
 import { AnimalsComponent } from './animals/animals.component';
 import { FruitsComponent } from './fruits/fruits.component';
+import { GenericComponent } from './generic/generic.component';
 import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
+  { path: 'generic/:id', component: GenericComponent },
   { path: 'animals', component: AnimalsComponent },
   { path: 'fruits', component: FruitsComponent }
 ];
