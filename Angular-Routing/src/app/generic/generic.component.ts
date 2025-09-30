9e97bb3968e07d1a72d8c37358e0d63aa4f06020
GabrielePapagni <159022047+GabrielePapagni@users.noreply.github.com>
2025-09-30 08:45:31 +0000

    Punto2

--- Angular-Routing/src/app/generic/generic.component.ts
+++ Angular-Routing/src/app/generic/generic.component.ts
@@ -9,25 +9,20 @@ import { ANIMALS, FRUITS, Emoji } from './models/emoji.model';
 })
 export class GenericComponent implements OnInit {
 
-  //Crea un vettore di tipo Emoji e lo inizializza
   genVect : Emoji[]= [{name:"", emoji:""}]; 
 
-  //Riceve l'oggetto ActivatedRoute come dependency injection
   constructor(private route: ActivatedRoute) {
-    //Gestisce i cambi di route con l'observable paramMap
     this.route.paramMap.subscribe(this.getRouterParam);
   }
   
   ngOnInit(): void {
   }
 
-  //Ogni volta che viene invocata la route tracks/:id, 
-  //l'observable paramMap richiama questo metodo
   getRouterParam = (params: ParamMap) =>
   {
-    let uri_param = params.get('id'); //Ottengo l'id dalla ParamMap
-    console.log (uri_param); //Stampo su console
-    //this.service.getTrack() 
+    let uri_param = params.get('id');
+    console.log (uri_param);
+
     if (uri_param == 'fruits') this.genVect = FRUITS;
     if (uri_param == 'animals') this.genVect = ANIMALS;
 
